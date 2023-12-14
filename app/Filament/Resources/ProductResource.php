3e69ac1fd7e3ca7765560fb34f4a3c05204a4551
chandraauliatama <chandraaulia4@gmail.com>
2023-12-15 00:18:28 +0700

    make product table grid

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -3,19 +3,20 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\ProductResource\Pages;
-use App\Filament\Resources\ProductResource\RelationManagers;
 use App\Models\Product;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
+use Filament\Support\Enums\FontWeight;
 use Filament\Tables;
+use Filament\Tables\Columns\Layout\Stack;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class ProductResource extends Resource
 {
     protected static ?string $model = Product::class;
+
+    protected static ?string $modelLabel = 'Produk';
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
@@ -44,27 +45,35 @@ class ProductResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
+            ->contentGrid([
+                'md' => 2,
+                'xl' => 3,
+            ])
             ->columns([
-                Tables\Columns\TextColumn::make('name')
-                    ->searchable(),
-                Tables\Columns\ImageColumn::make('image'),
-                Tables\Columns\TextColumn::make('sell_price')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('capital_price')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('stock')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                Stack::make([
+                    Tables\Columns\ImageColumn::make('image')
+                        ->columnSpanFull()
+                        ->alignCenter()
+                        ->width('full')
+                        ->height(200),
+                    Tables\Columns\TextColumn::make('name')
+                        ->weight(FontWeight::Bold)
+                        ->size('md')
+                        ->searchable(),
+                    Tables\Columns\TextColumn::make('sell_price')
+                        ->prefix('Harga Jual: Rp. ')->color('success')
+                        ->numeric()->sortable(),
+                    Tables\Columns\TextColumn::make('capital_price')
+                        ->prefix('Harga Modal: Rp. ')->color('danger')
+                        ->numeric()->sortable(),
+                    Tables\Columns\TextColumn::make('stock')
+                        ->prefix('Stok: ')
+                        ->numeric()->sortable(),
+                    Tables\Columns\TextColumn::make('updated_at')
+                        ->prefix('Terakhir Diubah: ')
+                        ->dateTime()->sortable()
+                        ->toggleable(isToggledHiddenByDefault: true),
+                ])->space(1.5),
             ])
             ->filters([
                 //
