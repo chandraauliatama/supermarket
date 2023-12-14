63c78d8926bf687e7e58d1e52190becc1f027553
chandraauliatama <chandraaulia4@gmail.com>
2023-12-14 23:57:26 +0700

    sale resource complete

--- app/Filament/Resources/SaleResource.php
+++ app/Filament/Resources/SaleResource.php
@@ -4,64 +4,110 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\SaleResource\Pages;
 use App\Filament\Resources\SaleResource\RelationManagers;
+use App\Models\Product;
 use App\Models\Sale;
+use Faker\Provider\ar_EG\Text;
 use Filament\Forms;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Placeholder;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Grouping\Group;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\HtmlString;
 
 class SaleResource extends Resource
 {
     protected static ?string $model = Sale::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $modelLabel = 'Penjualan';
+
+    protected static ?string $navigationIcon = 'heroicon-o-banknotes';
+
+    public static function canEdit(Model $record): bool
+    {
+        return false;
+    }
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('sell_price')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('capital_price')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('sale_count')
-                    ->required()
-                    ->numeric()
-                    ->default(1),
+                Repeater::make('sales')
+                    ->columnSpanFull()
+                    ->grid(2)
+                    ->addActionLabel('Tambah Produk')
+                    ->schema([
+                        Select::make('product')
+                            ->options(Product::where('stock', '>', 0)->pluck('name', 'id'))
+                            ->required()->live()->searchable(),
+                        TextInput::make('quantity')
+                            ->visible(fn(Get $get) => $get('product'))
+                            ->maxValue(fn(Get $get) => Product::find($get('product'))?->stock)
+                            ->required()->integer()->live()->minValue(1)->default(1),
+                        Placeholder::make('p_total')->hiddenLabel()
+                            ->visible(fn(Get $get) => $get('product') && $get('quantity'))
+                            ->content(function (Get $get, Set $set) {
+                                $product = Product::find($get('product'));
+                                $set('total', $product->sell_price * $get('quantity'));
+                                return "Total: Rp. " . ($product->sell_price * $get('quantity'));
+                            }),
+                        Hidden::make('total')
+                            ->default(0)->required()
+                    ]),
+
+                Placeholder::make('total_shop')
+                    ->hiddenLabel()->columnSpanFull()
+                    ->visible(fn(Get $get) => $get('sales'))
+                    ->content(function(Get $get) {
+                        $totals = 0;
+
+                        foreach($get('sales') as $sale) {
+                            $totals += $sale['total'];
+                        }
+
+                        return new HtmlString("<h1 class='text-2xl font-bold'>Total Belanjaan: Rp. {$totals}</h1>");
+                    })
             ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
+            ->defaultGroup('created_at')
+            ->groups([
+                Group::make('created_at')
+                    ->label('Waktu Transaksi')
+                    ->collapsible(true)
+            ])
             ->columns([
                 Tables\Columns\TextColumn::make('name')
+                    ->label('Name Produck')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('sell_price')
-                    ->numeric()
-                    ->sortable(),
+                    ->label('Harga Jual')->prefix('Rp. ')
+                    ->numeric()->sortable(),
                 Tables\Columns\TextColumn::make('capital_price')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('sale_count')
-                    ->numeric()
-                    ->sortable(),
+                    ->label('Harga Modal')->prefix('Rp. ')
+                    ->numeric()->sortable(),
+                Tables\Columns\TextColumn::make('quantity')
+                    ->numeric()->sortable(),
+                Tables\Columns\TextColumn::make('keuntungan')
+                    ->prefix('Rp. ')
+                    ->getStateUsing(fn(Sale $record) => ($record->capital_price - $record->sale_price) * $record->quantity),
                 Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                    ->label('Waktu Transaksi')
+                    ->dateTime()->sortable()
+                    ->toggleable(isToggledHiddenByDefault: false),
             ])
             ->filters([
                 //
@@ -71,7 +117,7 @@ class SaleResource extends Resource
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                    // Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
     }
--- app/Filament/Resources/SaleResource/Pages/CreateSale.php
+++ app/Filament/Resources/SaleResource/Pages/CreateSale.php
@@ -3,10 +3,35 @@
 namespace App\Filament\Resources\SaleResource\Pages;
 
 use App\Filament\Resources\SaleResource;
+use App\Models\Product;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateSale extends CreateRecord
 {
     protected static string $resource = SaleResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $transactionTime = now();
+
+        foreach ($data['sales'] as $sale) {
+            $product = Product::find($sale['product']);
+
+            $successSale = static::getModel()::create([
+                'name' => $product->name,
+                'sell_price' => $product->sell_price,
+                'capital_price' => $product->capital_price,
+                'quantity' => $sale['quantity'],
+                'created_at' => $transactionTime,
+                'updated_at' => $transactionTime,
+            ]);
+            
+            $product->stock = $product->stock - $sale['quantity'];
+            $product->save();
+        }
+
+        return $successSale;
+    }
 }
